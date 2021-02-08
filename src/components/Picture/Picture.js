c87046bf753768cca132bdfa31dd3c29785ffab5
Ancher Myumyun <anchertunchar@gmail.com>
2021-02-08 12:51:52 +0200

    Update Picture.js

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -5,7 +5,7 @@ import Box from '@material-ui/core/Box';
 export default function Picture({imageUrl}) {
     return (
       <Box color="text.primary" clone>
-          <img alt='test' src={imageUrl} style={{width: '100%', maxWidth: '500px'}}/>
+          <img alt='test' src={imageUrl} style={{width: '100%', maxWidth: '500px'}}/> //TODO input clarifai api output and input
       </Box>
     );
   }
